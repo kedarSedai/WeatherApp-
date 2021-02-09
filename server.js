b6e84598312c847555c397563c4ce9a0a954c07c
kedar sedai <kedar.sedai05@gmail.com>
2021-02-09 08:31:36 +0545

    added dotenv file and databse connection but connection is not success

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+//importing routes 
+const weatherRoute = require('./routes/weatherData');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+mongoose
+    .connect(process.env.DB_CONNECTION, {useCreateIndex:true, useNewUrlParser:true, useUnifiedTopology:true})
+    .then(() => console.log('Database connected'))
+    .catch(err => console.log('Database not connected'));
+
+app.use('/get', weatherRoute);
 
 app.get('/', (req, res) => {
     res.send('Server is running');
