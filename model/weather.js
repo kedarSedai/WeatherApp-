d0d8be7e477e954b4f166c7ea58b803722a85d01
kedar sedai <kedar.sedai05@gmail.com>
2021-02-10 08:12:45 +0545

    DB connection success & added ejs file

---
+++ model/weather.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const weatherData = new mongoose.Schema({
+
+});
+
+module.exports = mongoose.model('WeatherData', weatherData)
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+const ejs = require('ejs');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
 
 //importing routes 
 const weatherRoute = require('./routes/weatherData');
@@ -13,15 +16,24 @@ const weatherRoute = require('./routes/weatherData');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+//Import database 
+const db = require('./dbSetup/dbSetup').mongoURI;
+
 mongoose
-    .connect(process.env.DB_CONNECTION, {useCreateIndex:true, useNewUrlParser:true, useUnifiedTopology:true})
+    .connect(db, {useCreateIndex:true, useNewUrlParser:true, useUnifiedTopology:true})
     .then(() => console.log('Database connected'))
     .catch(err => console.log('Database not connected'));
 
-app.use('/get', weatherRoute);
+app.get('/ejs', (req, res) => {
+    res.render('index')
+})
 
+//test route
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
+
+app.use('/get', weatherRoute);
+
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
